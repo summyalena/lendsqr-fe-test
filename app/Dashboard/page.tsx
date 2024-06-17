d16759431c164900d9d3b8c23975f5a114030207
duanedave <duanedave17@gmail.com>
2024-06-18 00:14:19 +0100

    Create Dasboard Navbar

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './page.module.scss';
+import Navbar from './Navbar/Navbar';
+
+
+function page() {
+  return (
+    <div>
+      <Navbar />
+      
+    </div>
+  )
+}
+
+export default page
