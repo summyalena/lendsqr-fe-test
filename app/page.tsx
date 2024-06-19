da8920f1d51537801311d7b544e4625def30863b
summyalena <summyalena@gmail.com>
2024-06-19 21:43:37 +0100

    Create Dashboard and add dashboard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import React from 'react';
+import Layout from '../components/Dashboard/Layout/Layout';
+import UserTag from '../components/ui/reusables/userTag/UserTag';
+import Table from '../components/Dashboard/Table/Table';
+import styles from '../app/Dashboard/page.module.scss';
+import ProtectedRoute from '@/components/utils/ProtectedRoute';
+import {AuthProvider} from '@/components/utils/context/authContext';
+import LayoutWithAuth from '@/components/ui/LayoutWithAuth';
+import Dashboard from '../components/Dashboard/Dashboard';
 
-export default function Home() {
+function page() {
   return (
-    <div>
-      Dashboard
-   </div>
+    // <LayoutWithAuth>
+      <ProtectedRoute>
+        <Dashboard />
+        </ProtectedRoute>
+    // </LayoutWithAuth>
   );
 }
+
+export default page;
