47bc9e3e5af2aa0b531242e9ae5ef7f969c6be7f
summyalena <summyalena@gmail.com>
2024-06-19 22:02:35 +0100

    Arrange types in a file

---
+++ components/utils/types.ts
@@ -0,0 +1,19 @@
+export interface User {
+  name: string;
+  username: string;
+  password: string;
+}
+
+export interface UserDetailsProps {
+  user: string;
+}
+
+export interface User {
+  id: number;
+  organization: string;
+  username: string;
+  email: string;
+  phone: string;
+  createdAt: string;
+  status: string;
+}
