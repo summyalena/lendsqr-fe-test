e19a083a1db3b5f3d6b6a420bb437dec04c3c2ca
summyalena <summyalena@gmail.com>
2024-06-19 22:00:04 +0100

    Create and style tags for dashboard

---
+++ components/ui/reusables/userTag/UserTag.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './UserTag.module.scss';
+import { UserActiveIcon } from '@/public/icons/icons';
+import { UsersLoanIcon } from '@/public/icons/icons';
+import { UsersSavingsIcon } from '@/public/icons/icons';
+
+const icons = {
+  activeUsers: UserActiveIcon,
+  loans: UsersLoanIcon,
+  savings: UsersSavingsIcon,
+};
+
+function UserTag({
+  tag,
+  figures,
+  iconType,
+}: {
+  tag: string;
+  figures: number;
+  iconType: 'activeUsers' | 'loans' | 'savings';
+}) {
+  const IconComponent = icons[iconType] as React.ComponentType<{ className: string }>;
+
+  return (
+    <div className={`${styles.tagContainer}`}>
+      <IconComponent className={styles.icon} />
+      <h2>{tag}</h2>
+      <p>{figures}</p>
+    </div>
+  );
+}
+
+export default UserTag;
