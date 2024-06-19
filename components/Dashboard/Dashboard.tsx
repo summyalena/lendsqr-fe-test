29a9b15bfa334134945a48e7c5577b9d6302144c
summyalena <summyalena@gmail.com>
2024-06-19 21:48:10 +0100

    Build Dasboard as a component

---
+++ components/Dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from './Layout/Layout';
+
+import styles from './page.module.scss';
+
+import Content from './Content/Content';
+
+function Dashboard() {
+  return (
+    <Layout>
+      <Content/>
+        </Layout>
+  );
+}
+
+export default Dashboard;
