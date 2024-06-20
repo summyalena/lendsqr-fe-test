1073ca732153bf2f0fe06a67fcc6db814dfe559f
summyalena <summyalena@gmail.com>
2024-06-20 10:37:16 +0100

    Create dynamic route for user details page

---
+++ app/user/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import UserDetailsPage from './[username]/page'
+
+function page() {
+  return (
+    <div>
+      {/* <UserDetailsPage username={''}/> */}
+      under construction
+    </div>
+  )
+}
+
+export default page
+
+
+// local:3000/signin
+// localhost:300/signin
+// localhost:3000
+// localhost:3000/users
+// localhost:3000/users/alena
