59b52507bedadfc2957f8de6493733dd8bf91775
summyalena <summyalena@gmail.com>
2024-06-20 22:02:48 +0100

    Style the user dynamic page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
-// import UserDetailsPage from './[username]/page'
+import Dashboard from '@/components/Dashboard/Dashboard'
 
 function page() {
   return (
     <div>
-      {/* <UserDetailsPage username={''}/> */}
-      under construction
+      <h1> Under Construction </h1>
     </div>
   )
 }
 
 export default page
-
-
-// local:3000/signin
-// localhost:300/signin
-// localhost:3000
-// localhost:3000/users
-// localhost:3000/users/alena
