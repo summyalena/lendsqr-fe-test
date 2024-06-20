562ebf8687413ff0de272965df75324fc04e538b
summyalena <summyalena@gmail.com>
2024-06-20 22:05:29 +0100

    Build filter functionality

--- components/ui/reusables/FilterModal/FilterModal.tsx
+++ components/ui/reusables/FilterModal/FilterModal.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import styles from './filtermodal.module.scss';
+
+import ApiContext from '@/components/utils/context/apiContext';
 
 type Props = {
   column: string;
@@ -8,103 +10,122 @@ type Props = {
 };
 
 function FilterModal({ column, onClose, onReset }: Props) {
+  const { filterDataByOptions } = useContext(ApiContext);
+
   const [filters, setFilters] = useState({
     organization: '',
     username: '',
     email: '',
     date: '',
     phoneNumber: '',
-    status: ''
+    status: '',
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
-    setFilters(prevFilters => ({ ...prevFilters, [name]: value }));
+    setFilters((prevFilters) => ({ ...prevFilters, [name]: value }));
   };
 
   const onResetFilter = () => {
     onReset();
   };
 
+  const handleFilter = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    filterDataByOptions(filters);
+    onClose();
+  };
+
   return (
-    <div className={styles.modal}>
+    <form
+      className={styles.modal}
+      onSubmit={handleFilter}
+      aria-label="Filter Modal"
+      id="filter-modal"
+      role="dialog"
+    >
       <div className={styles.modalContent}>
         <div className={styles.filterFields}>
           <span>
             <p>Organization</p>
-          <select
-            name="organization"
-            value={filters.organization}
-            onChange={handleChange}
-          >
-            <option value="">Select</option>
-            <option value="Lendsqr">Lendsqr</option>
-            <option value="Irorun">Irorun</option>
-            <option value="Lendstar">Lendstar</option>
+            <select
+              name="organization"
+              value={filters.organization}
+              onChange={handleChange}
+            >
+              <option value="">Select</option>
+              <option value="Lendsqr">Lendsqr</option>
+              <option value="Irorun">Irorun</option>
+              <option value="Lendstar">Lendstar</option>
             </select>
           </span>
           <span>
             <p>Username</p>
-          <input
-            type="text"
-            name="username"
-            placeholder="User"
-            value={filters.username}
-            onChange={handleChange}
+            <input
+              type="text"
+              name="username"
+              placeholder="User"
+              value={filters.username}
+              onChange={handleChange}
             />
           </span>
           <span>
             <p>Email</p>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={filters.email}
-            onChange={handleChange}
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={filters.email}
+              onChange={handleChange}
             />
           </span>
           <span>
             <p>Date</p>
-          <input
-            type="date"
-            name="date"
-            placeholder="Date"
-            value={filters.date}
-            onChange={handleChange}
+            <input
+              type="date"
+              name="date"
+              placeholder="Date"
+              value={filters.date}
+              onChange={handleChange}
             />
           </span>
           <span>
             <p>Phone Number</p>
-          <input
-            type="text"
-            name="phoneNumber"
-            placeholder="Phone Number"
-            value={filters.phoneNumber}
-            onChange={handleChange}
+            <input
+              type="text"
+              name="phoneNumber"
+              placeholder="Phone Number"
+              value={filters.phoneNumber}
+              onChange={handleChange}
             />
           </span>
           <span>
             <p>Status</p>
-          <select
-            name="status"
-            value={filters.status}
-            onChange={handleChange}
-          >
-            <option value="">Select Status</option>
-            <option value="active">Active</option>
-            <option value="inactive">Inactive</option>
-            <option value="pending">Pending</option>
-            <option value="blacklisted">Blacklisted</option>
+            <select
+              name="status"
+              value={filters.status}
+              onChange={handleChange}
+            >
+              <option value="">Select Status</option>
+              <option value="active">Active</option>
+              <option value="inactive">Inactive</option>
+              <option value="pending">Pending</option>
+              <option value="blacklisted">Blacklisted</option>
             </select>
-            </span>
+          </span>
         </div>
         <div className={styles.modalButtons}>
-          <button onClick={onResetFilter}>Reset</button>
-          <button onClick={onClose}>Filter</button>
+          <button onClick={onResetFilter} type="button">
+            Reset
+          </button>
+          <button type="submit">Filter</button>
         </div>
       </div>
-    </div>
+    </form>
   );
-};
+}
 
 export default FilterModal;
